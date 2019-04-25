61ff65283a36d792da29bb587293f1772ef49b0f
Somebodyzm <zm199551@qq.com>
2019-04-25 15:17:33 +0800

    Update isValid.py

--- isValid.py
+++ isValid.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        stack =[] #空栈
-        dic ={')':'(','}':'{',']':'['} # 字典，
-        for char in s :
-            if char in dic :
-                top = stack.pop() if stack else '#'
-                if top != dic[char] :
+        stack =[]  #初始化栈stack
+        dic ={')':'(','}':'{',']':'['}  #建立字典dict，key为右括号，value为左括号
+        for char in s :  #遍历输入字符串s的每个字符char
+            if char in dic :  #如果char在dict里（即为右括号）
+                top = stack.pop() if stack else '#'  #弹出栈顶字符top
+                if top != dic[char] :  #如果栈顶字符不是对应符号的左括号，返回无效
                     return False
-            else :
+            else :  #  如果char不在dict里（即为左括号），将char压入栈中
                 stack.append(char)
-        return not stack
+        return not stack  #最后栈中还有字符，返回无效
